09d76256582dac72510bd02b5487357949cf7d64
MartinMandzak <mandzak.martin3@gmail.com>
2023-12-05 14:31:07 +0100

    working now?

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
-const http_1 = __importDefault(require("http"));
-const server = http_1.default.createServer((req, res) => {
+var http_1 = require("http");
+var server = http_1.default.createServer(function (req, res) {
     res.write('Hello, World!');
     res.end();
 });
-const test_port = 3000;
-const domain = 'webska.net';
-server.listen(domain, () => {
-    console.log(`Server is running at http://localhost:${domain}`);
+var test_port = 3000;
+var domain = 'webska.net';
+server.listen(domain, function () {
+    console.log("Server is running at http://localhost:".concat(domain));
 });
