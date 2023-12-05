69e75213dc809fbc57dec305feb63374d764470f
MartinMandzak <mandzak.martin3@gmail.com>
2023-12-05 14:25:33 +0100

    trying something new

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const http_1 = __importDefault(require("http"));
+const server = http_1.default.createServer((req, res) => {
+    res.write('Hello, World!');
+    res.end();
+});
+const test_port = 3000;
+const domain = 'webska.net';
+server.listen(domain, () => {
+    console.log(`Server is running at http://localhost:${domain}`);
+});
--- index.ts
+++ index.ts
@@ -1,2 +1,13 @@
+import http from "http"
 
+const server = http.createServer((req, res) => {
+ res.write('Hello, World!');
+ res.end();
+});
+
+const test_port = 3000;
+const domain = 'webska.net';
+server.listen(domain, () => {
+ console.log(`Server is running at http://localhost:${domain}`);
+});
 
