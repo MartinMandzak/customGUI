68c2396dda193defb1deedf16036b82b1a59ca37
MartinMandzak <mandzak.martin3@gmail.com>
2024-03-26 12:09:15 +0100

    uwu

--- docs/index.js
+++ docs/index.js
@@ -1,3 +1,10 @@
 var mainScreenTitle = document.getElementById("title");
+var mainScreenSubTitle = document.getElementById("subtitle");
 
 mainScreenTitle.innerText = "Jimble Beta";
+mainScreenTitle.style.fontSize = "30px";
+mainScreenTitle.style.textAlign = "center";
+
+mainScreenSubTitle.innerText = "coming soon!";
+mainScreenSubTitle.style.fontSize = "15px";
+mainScreenSubTitle.style.textAlign = "center";
