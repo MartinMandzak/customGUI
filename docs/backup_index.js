f397f8aea790cf1df5b44ccc8147a04c8667ab03
MartinMandzak <mandzak.martin3@gmail.com>
2024-03-26 14:22:09 +0100

    trying out typescript

--- docs/backup_index.js
+++ docs/backup_index.js
@@ -1,8 +1,10 @@
 var mainScreenTitle = document.getElementById("title");
 var mainScreenSubTitle = document.getElementById("subtitle");
+
 mainScreenTitle.innerText = "Jimble Beta";
 mainScreenTitle.style.fontSize = "30px";
 mainScreenTitle.style.textAlign = "center";
-mainScreenSubTitle.innerText = "Coming soon!";
+
+mainScreenSubTitle.innerText = "coming soon!";
 mainScreenSubTitle.style.fontSize = "15px";
 mainScreenSubTitle.style.textAlign = "center";
--- docs/index.ts
+++ docs/index.ts
@@ -1,8 +1,11 @@
-var mainScreenTitle = document.getElementById("title");
-var mainScreenSubTitle = document.getElementById("subtitle");
+
+const mainScreenTitle = document.getElementById("title") as HTMLElement;
+const mainScreenSubTitle = document.getElementById("subtitle") as HTMLElement;
+
 mainScreenTitle.innerText = "Jimble Beta";
 mainScreenTitle.style.fontSize = "30px";
 mainScreenTitle.style.textAlign = "center";
+
 mainScreenSubTitle.innerText = "Coming soon!";
 mainScreenSubTitle.style.fontSize = "15px";
 mainScreenSubTitle.style.textAlign = "center";
